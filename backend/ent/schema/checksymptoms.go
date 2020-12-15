e28198aef013789ef45e83202514eb68f59f0a5b
jamezanatoss <65354513+jamezanatoss@users.noreply.github.com>
2020-12-15 19:26:41 +0700

    ทำ Entity สำหรับ ระบบบันทึกการนัดตรวจอาการ -close #21

---
+++ backend/ent/schema/checksymptoms.go
@@ -0,0 +1,18 @@
+package schema
+
+import "github.com/facebookincubator/ent"
+
+// Checksymptoms holds the schema definition for the Checksymptoms entity.
+type Checksymptoms struct {
+	ent.Schema
+}
+
+// Fields of the Checksymptoms.
+func (Checksymptoms) Fields() []ent.Field {
+	return nil
+}
+
+// Edges of the Checksymptoms.
+func (Checksymptoms) Edges() []ent.Edge {
+	return nil
+}
