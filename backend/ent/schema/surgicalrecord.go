27402accc6b56f4b91a5aedd99cadb103050d6e0
Ktsd <oneirio115@gmail.com>
2020-12-15 18:50:06 +0700

    ทำ Entity สำหรับระบบบันทึกการนัดผ่าตัด - close #15

---
+++ backend/ent/schema/surgicalrecord.go
@@ -0,0 +1,18 @@
+package schema
+
+import "github.com/facebookincubator/ent"
+
+// Surgicalrecord holds the schema definition for the Surgicalrecord entity.
+type Surgicalrecord struct {
+	ent.Schema
+}
+
+// Fields of the Surgicalrecord.
+func (Surgicalrecord) Fields() []ent.Field {
+	return nil
+}
+
+// Edges of the Surgicalrecord.
+func (Surgicalrecord) Edges() []ent.Edge {
+	return nil
+}
