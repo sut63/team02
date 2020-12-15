959eac993c739b4f705be2dcc8687e6b799b9c53
lllToTolll <69581325+to63@users.noreply.github.com>
2020-12-15 20:43:37 +0700

    ทำ Entity  ของระบบบันทึกโรคกระดูก - close#19

---
+++ backend/ent/schema/bonediseae.go
@@ -0,0 +1,18 @@
+package schema
+
+import "github.com/facebookincubator/ent"
+
+// Bonediseae holds the schema definition for the Bonediseae entity.
+type Bonediseae struct {
+	ent.Schema
+}
+
+// Fields of the Bonediseae.
+func (Bonediseae) Fields() []ent.Field {
+	return nil
+}
+
+// Edges of the Bonediseae.
+func (Bonediseae) Edges() []ent.Edge {
+	return nil
+}
