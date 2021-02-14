625cf49cef56fc2e3264d89f83849c2edbea8ce3
lllToTolll <69581325+to63@users.noreply.github.com>
2021-02-15 01:49:48 +0700

    ลบ comment // sweet alert เพื่อให้กำหนดเวลาการแสดง sweet alert

--- frontend/plugins/welcome/src/components/Bonedisease/Bonedisease.tsx
+++ frontend/plugins/welcome/src/components/Bonedisease/Bonedisease.tsx
@@ -90,7 +90,7 @@ export default function Insurance() {
     toast: true,
     position: 'top-end',
     showConfirmButton: false,
-    //timer: 3000,
+    timer: 3000,
     timerProgressBar: true,
     didOpen: toast => {
       toast.addEventListener('mouseenter', Swal.stopTimer);
